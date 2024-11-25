a9856d1437844057e27ccecd296939105d1333a0
Hasibur Rahman <hasibhr31@gmail.com>
2024-11-25 18:51:04 +0600

    add scheduler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/hasib-003/newsLetter/config"
 	"github.com/hasib-003/newsLetter/routes"
+	"github.com/hasib-003/newsLetter/scheduler"
 )
 
 func main() {
@@ -14,6 +15,8 @@ func main() {
 
 	routes.RegisterUserRoutes(r)
 	routes.RegisterNewsRoutes(r)
+
+	scheduler.StartScheduler()
 
 	r.Run(":8080")
 }
